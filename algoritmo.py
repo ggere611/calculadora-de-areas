c7bd35cf9945ce9c909c492720446dd923c71b70
geremias <gomez.ge611@gmail.com>
2024-12-29 14:07:10 -0300

    realizo una calculadora de areas ingresando el radio

---
+++ algoritmo.py
@@ -0,0 +1,8 @@
+#1 Solicitasr el radio del circulo
+import math
+radio=float(input("Por favor, ingrese el radio del circulo:" ))
+
+#2 calcular el area del circulo
+area=math.pi*(radio**2)
+#3 mostrar en pantalla el area del circulo en pantalla
+print("El area del circulo con radio", radio,"es", area)
